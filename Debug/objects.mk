7043c33588d9cd69f7a4a6b66740e013e933f5ad
Yuncong Chen <cyc3700@gmail.com>
2013-04-04 16:30:11 -0700

    remove Debug

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lopencv_calib3d -lopencv_flann -lopencv_imgproc -lopencv_photo -lopencv_highgui -lopencv_contrib -lopencv_core -lboost_system-mt -lboost_filesystem-mt -lboost_thread-mt -lpcl_visualization -lpcl_io -lpcl_ml -lpcl_features -lpcl_search -lpcl_filters -lpcl_segmentation -lpcl_sample_consensus -lpcl_surface -lpcl_common -lvtkRendering -lopencv_flann -lvtkHybrid -lvtkCommon -lvtkGraphics -lvtkFiltering
-
