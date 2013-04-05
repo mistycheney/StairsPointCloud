fee6e06ce3344501b808645ab063c071bba11a4c
Yuncong Chen <cyc3700@gmail.com>
2013-04-04 23:39:21 -0700

    remove old files

--- runLIBELAS.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BINPATH=/Users/yuncong/Documents/libelas
-IMGPATH=/Users/yuncong/Documents/workspace/StairsPointCloud/staircase_new
-
-IMGID=$1
-
-$BINPATH/elas $IMGPATH/top${IMGID}_rect_rot.pgm $IMGPATH/bottom${IMGID}_rect_rot.pgm 
